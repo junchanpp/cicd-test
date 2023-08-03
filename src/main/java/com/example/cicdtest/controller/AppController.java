282d9608b413b00baa838d0b58e0a68e21bad4cd
9pruruluru2 <9pruruluru2@naver.com>
2023-08-04 05:22:40 +0900

    add appspec.yml

--- src/main/java/com/example/cicdtest/controller/AppController.java
+++ src/main/java/com/example/cicdtest/controller/AppController.java
@@ -1,2 +1,12 @@
-package com.example.cicdtest.controller;public class AppController {
+package com.example.cicdtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+  @GetMapping("/")
+  public String index() {
+    return "Hello, world!";
+  }
 }
